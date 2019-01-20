abf85ea14cb039f40bb6df76198bbd6bccdacc9f
Quentin Bazin <quent42340@gmail.com>
2019-01-20 20:24:50 +0100

    Android support removed.

--- droid/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-APP_PLATFORM := android-10
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-#APP_STL := stlport_static
-APP_STL := gnustl_shared
-APP_CPPFLAGS := -std=c++11
-
-NDK_TOOLCHAIN_VERSION := 4.8
