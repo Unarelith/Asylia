f46e7fce6b1c94dbe4142e23ea343018736cbbbb
Quentin Bazin <quent42340@gmail.com>
2019-02-04 05:50:16 +0100

    Removed trailing whitespaces.

--- data/events/event1/main.lua
+++ data/events/event1/main.lua
@@ -2,21 +2,21 @@ event1 = {}
 
 event1.init = function()
 	event1.update = function()
-		
+
 	end
-	
+
 	event1.render = function()
-		
+
 	end
-	
+
 	event1.action = function()
-		
+
 	end
-	
+
 	event1.collisionAction = function()
 		CharacterManager.player():changeMap(0, 1, 0, 20, 27, 0)
 	end
-	
+
 	event1.movements = {
 		function() end
 	}
--- data/events/event2/main.lua
+++ data/events/event2/main.lua
@@ -2,21 +2,21 @@ event2 = {}
 
 event2.init = function()
 	event2.update = function()
-		
+
 	end
-	
+
 	event2.render = function()
-		
+
 	end
-	
+
 	event2.action = function()
-		
+
 	end
-	
+
 	event2.collisionAction = function()
 		CharacterManager.player():changeMap(0, 0, 0, 8, 25, 0)
 	end
-	
+
 	event2.movements = {
 		function() end
 	}
